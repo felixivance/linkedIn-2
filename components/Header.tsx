65175d04f20ef362166e144108011bad2f763baa
Felix <felixrunye@gmail.com>
2024-07-10 22:38:43 +0300

    header customization

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className="flex">
+      <Image
+        className="rounded-lg"
+        src="https://links.papareact.com/b3z"
+        width={40}
+        height={40}
+        alt="logo"
+      />
+    </div>
+  );
 };
 
 export default Header;
