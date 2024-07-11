8ae4aa960ba1248735af8ddbd01fc88b8bb214ac
Felix <felixrunye@gmail.com>
2024-07-11 16:45:05 +0300

    updated comments and posts

---
+++ mongodb/models/post.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema, Document, models, Model} from "mongoose";
+import { IComment, ICommentBase } from "./comment";
+import { IUser } from "@/types/user";
+
+export interface IPostBase{
+    user: IUser;
+    text: string;
+    imageUrl?:string;
+    comments?: IComment[];
+    likes?:string[];
+}
+
+export interface IPost extends IPostBase, Document{
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+// define document methods  ( for each instance of a post )
+
+interface IPostMethods{
+    likePost(userId: string): Promise<void>;
+    unlikePost(userId:string): Promise<void>;
+    commentOnPost(comment: ICommentBase): Promise<void>;
+    getAllComments(): Promise<IComment[]>;
+    removePost(): Promise<void>;
+}
+
+interface IPostStatistics{
+    getAllPosts(): Promise<IPostDocument[]>;
+}
+
+export interface IPostDocument extends IPost, IPostMethods{} // singular instance of a post
+
+interface IPostModel extends IPostStatistics, Model<IPostDocument>{} // all posts
+
+const PostSchema = new Schema<IPostDocument>({
+    user: {
+        userId: { type: String, required:true },
+        userImage: { type: String, required:true},
+        firstName: { type: String, required: true},
+        lastName: { type: String}
+    },
+    text: { type: String, required: true},
+    imageUrl: { type: String },
+    comments: { type: [Schema.Types.ObjectId], ref: "Comment", default: []},
+    likes: { type: [String]}
+},{
+    timestamps: true
+})
+
+PostSchema.methods.likePost = async function (userId: string){
+    try{
+        await this.updateOnt({ $addToSet: { likes: userId}});
+    }catch(error){
+        console.log("Failed to like post", error);
+    }
+}
