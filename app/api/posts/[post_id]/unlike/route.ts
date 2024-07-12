eef7bc8a183407e63f4873a6470faf25eee5ffcf
Felix <felixrunye@gmail.com>
2024-07-12 14:35:37 +0300

    unlike post route completion

---
+++ app/api/posts/[post_id]/unlike/route.ts
@@ -0,0 +1,31 @@
+import connectDB from "@/mongodb/db";
+import { Post } from "@/mongodb/models/post";
+import { NextResponse } from "next/server"
+
+export interface UnlikePostRequestBody {
+    userId: string;
+  }
+
+export async function POST(request: Request, { params }: { params: {post_id : string}}){
+
+    try{
+        await connectDB();
+
+        const { userId } : UnlikePostRequestBody = await request.json();
+
+        const post = await Post.findById(params.post_id);
+
+        if(!post){
+            return NextResponse.json({ error: " Post not found "}, { status: 404})
+        }
+
+        await post.unlikePost(userId);
+
+        return NextResponse.json({ message: "post unliked successfully"})
+    }catch(err: any){
+        return NextResponse.json(
+            { error: "An error occurred fetching the post ", err},
+            { status: 500}
+        )
+    }
+}
